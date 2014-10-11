6b168e81450f03ee63eb5c4615d14ac2279ffd4b
YRyoma <zaki.ryo.0525@gmail.com>
2014-10-10 22:54:08 -0700

    画像表示できた（リンク付与未完）

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
 	belongs_to :member
 	belongs_to :category
+	belongs_to :photo
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,4 @@
 class Photo < ActiveRecord::Base
 	belongs_to :member
+	has_many :articles
 end
