b5344538b6f6b618a48f109dd50075555b625b00
shoyasono <shoya.ono.125@gmail.com>
2014-11-18 12:05:38 -0800

    cssを修正

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -5,6 +5,6 @@ class ArticleController < ApplicationController
   end
   def show
   	@article = Article.find(params[:id])
-    @articles = Article.where(category_id: '1').limit(5)
+    @articles = Article.where(category_id: @article.category_id).limit(5)
   end
 end
