5a086ce95002166fe46252c0984094e500e4e7d5
YRyoma <zaki.ryo.0525@gmail.com>
2014-11-20 01:07:27 -0800

    SEO1

--- app/controllers/article_controller.rb
+++ app/controllers/article_controller.rb
@@ -2,9 +2,11 @@ class ArticleController < ApplicationController
 
   def index
   	@category = Category.find(params[:id])
+  	render :layout => 'articles'
   end
   def show
   	@article = Article.find(params[:id])
     @articles = Article.where(category_id: @article.category_id).limit(5)
+    render :layout => 'article'
   end
 end
